ba65d292cb30b9a3f8c4d6f49818a5e9355809e9
Andy <andy.halim2704@gmail.com>
2021-02-19 21:19:19 +0700

    fix profile page & design

--- onmarcy/app/src/main/java/com/android/onmarcy/ProfileFragment.java
+++ onmarcy/app/src/main/java/com/android/onmarcy/ProfileFragment.java
@@ -16,6 +16,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
