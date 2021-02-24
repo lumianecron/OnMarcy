2cbf06e827bb59395b5c3db8595faf2eabfa9023
Andy <andy.halim2704@gmail.com>
2021-02-24 12:26:57 +0700

    changes

---
+++ onmarcy/app/src/main/java/com/android/onmarcy/InstagramActivity.java
@@ -0,0 +1,13 @@
+package com.android.onmarcy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstagramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instagram);
+    }
+}
